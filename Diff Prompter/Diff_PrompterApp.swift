9669e0caaba7d27b12ee4672118b222715e96bc6
Adrian Price <adrian.price@rea-group.com>
2026-04-25 18:14:03 +1000

    Initial Commit

---
+++ Diff Prompter/Diff_PrompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diff_PrompterApp.swift
+//  Diff Prompter
+//
+//  Created by Adrian Price on 25/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Diff_PrompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Diff PrompterTests/Diff_PrompterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Diff_PrompterTests.swift
+//  Diff PrompterTests
+//
+//  Created by Adrian Price on 25/4/2026.
+//
+
+import Testing
+@testable import Diff_Prompter
+
+struct Diff_PrompterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
